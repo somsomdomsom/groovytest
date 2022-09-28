9fd8da0bdc066e1525d090efb354119a9f8e5552
somsom <76727055+somsomdomsom@users.noreply.github.com>
2022-09-28 15:50:19 +0530

    Add files via upload

--- vars/somsom.groovy
+++ vars/somsom.groovy
@@ -1,5 +1,8 @@
 #!/usr/bin/env groovy
-
-def call(String name = 'human') {
+def call(String name) {
     echo "Hello, ${name}."
 }
+
+// def add(int a, int b){
+//     echo "Addition is ${a + b}"
+// }
